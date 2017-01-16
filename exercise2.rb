254ba4cb253d37086a53b65de50bfd42e914294d
Dgibbins <nayr_ryan@hotmail.com>
2017-01-16 15:29:41 -0500

    Excercise 2

---
+++ exercise2.rb
@@ -0,0 +1,16 @@
+#How would you calculate a good tip for a 55 dollar meal? Use puts to print the answer.
+
+
+puts "A $55 meal would have a 20% tip, this would equate to $#{55*0.20}."
+
+#Try adding a string and an integer with the + operator. What happens? Find a way to convert the integer into a string first and use puts to print the result.
+
+puts 5.to_s + " is concatenated to me!!!!"
+
+#Try outputting the result of 45628 multiplied by 7839 in a sentence by using string interpolation.
+
+puts " When you multiply 45628 by 7839 the value is #{45628*7839}"
+
+#What's the value of the expression (10 < 20 && 30 < 20) || !(10 == 11)? Try figuring it out on your own before typing it in.
+
+true
